53e665f1a9055b39a31e46681ab7f647151f7a2d
Himalaya <yugmani@yahoo.com>
2020-05-29 23:24:27 -0700

    simple time clock is set

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # simpleClock
+
+* Use methods of the Date object to obtain the hours, minutes, and seconds.
+* Set the variable so that am or pm may be displayed after the time.
+* The hours, minutes, and seconds are adjusted so that they will display like a typical 12 hour digital clock.
+* The hours, minutes, seconds and ap variables are combined and displayed as the new innerHTML for the div element.
+* Call the function to start the process
+* Call script everysecond calling the setTimeout() method.
---
+++ clock.js
@@ -0,0 +1,21 @@
+
+function startclock(){
+    let thetime = new Date(),
+    hours = thetime.getHours(),
+    mins = thetime.getMinutes(),
+    secn = thetime.getSeconds(),
+    ap = (hours >= 12) ? "p.m." : "a.m.",
+    clock_div = document.getElementById("my_clock");    
+    hours = (hours >= 13) ? hours -= 12 : hours;
+    hours = (hours < 1) ? 12 : hours;
+    mins = (mins < 10) ? "0" + mins : mins;
+    secn = (secn < 10) ? "0" + secn: secn; 
+    clock_div.innerHTML = "<h2>"+hours + ":" + mins + ":"+ secn + " " + ap + "</h2>";
+
+    setTimeout(function(){startclock(); }, 1000);
+
+}
+
+
+startclock();
+
